0bd535f5992c15d6385429132e7e16ef4910fcce
AlexanderTheWorst <alexander.backman@elev.ga.ntig.se>
2025-08-20 14:16:37 +0200

    Update

--- website/.svelte-kit/adapter-node/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.uDXMbuLe.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/BhrWO1zB.js","_app/immutable/chunks/Bi19Avw7.js"];
-export const stylesheets = ["_app/immutable/assets/0.DNyseGkR.css"];
-export const fonts = [];
