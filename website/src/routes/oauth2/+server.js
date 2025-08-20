a4efdd64329aa272cfc7c12a865b9f5f847894fb
AlexanderTheWorst <alexander.backman@elev.ga.ntig.se>
2025-08-20 22:04:07 +0200

    Rahh

--- website/src/routes/oauth2/+server.js
+++ website/src/routes/oauth2/+server.js
@@ -1,8 +1,13 @@
 import { json, redirect } from '@sveltejs/kit';
 
-function getOAuth2Link(request) {
-    console.log(request.url)
-}
+const { 
+    DISCORD_OAUTH2_CLIENT_ID: discord_client_id = undefined,
+    DISCORD_OAUTH2_CLIENT_SECRET: discord_client_secret = undefined,
+    DISCORD_OAUTH2_REDIRECT_URI: discord_redirect_uri = undefined,
+    DISCORD_OAUTH2_REDIRECT_URI_DEV: discord_redirect_uri_dev = undefined,
+} = process.env;
+
+console.log(discord_client_id, discord_client_secret, discord_redirect_uri, discord_redirect_uri_dev)
 
 function extractQueries(searchParams) {
     let entries = searchParams.entries();
@@ -11,8 +16,22 @@ function extractQueries(searchParams) {
     return lookup;
 }
 
+function getDiscordOAuth2Link(request) {
+    const { hostname } = request.url;
+
+    let redirect_uri = (["localhost", "127.0.0.1"]).includes(hostname) ? discord_redirect_uri_dev : discord_redirect_uri;
+
+    console.log(redirect_uri);
+
+    return (`
+    https://discord.com/oauth2/authorize?client_id=${discord_client_id}&response_type=code&redirect_uri=${redirect_uri}&scope=identify+guilds
+    `).trim();
+}
+
 function discordOAuth2(request, code) {
-    let oauth2URI = getOAuth2Link(request);
+    let oauth2Link = getDiscordOAuth2Link(request);
+
+    if (!code) return redirect(307, oauth2Link);
 
     return new Response(
         new URLSearchParams({
@@ -35,6 +54,7 @@ export async function GET(request) {
     const { url } = request;
     const query = extractQueries(url.searchParams);
     const { platform, code } = query;
+    console.log(query);
 
     switch (platform.toLowerCase()) {
         case null: return redirect("/");
--- website/vite.config.js
+++ website/vite.config.js
@@ -1,6 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { config} from 'dotenv';
+
+config({ path: '../.env' }); // Load .env
 
 export default defineConfig({
 	server: {
