2406981ee97b28d12e590f9ad5068c2ada7fc808
AlexanderTheWorst <alexander.backman@elev.ga.ntig.se>
2025-08-20 13:15:41 +0200

    e

--- website/index.js
+++ website/index.js
@@ -19,7 +19,7 @@ if (!isInDocker()) {
 
 app.get("/", (req, res) => {
     console.log(process.env.DOCKER, process.env.DOCKER_NAME, process.env.ROBLOX_OAUTH2_TEST)
-    res.send(process.env.DOCKER ? "Docker!" : "Not a docker!", process.env.DOCKER_NAME);
+    res.send(`${process.env.DOCKER ? "Docker!" : "Not a docker!"} ${process.env.DOCKER_NAME ? process.env.DOCKER_NAME : "No name for docker!"}`);
 })
 
 app.listen(3000, () => {
